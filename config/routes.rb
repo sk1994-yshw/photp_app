b7f1fc6f1bd6a5ee10f7011dbf8c88396cb76258
Ubuntu <ubuntu@ip-172-31-22-238.us-east-2.compute.internal>
2020-05-17 06:42:03 +0000

    20200517_create_home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  
+  root 'static_pages#home'
+  get "/home" => "static_pages#home"
+  get "/help" => "static_pages#help"
+  get "/about" => "static_pages#about"
+  get "/signup" =>  "users#new"
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
